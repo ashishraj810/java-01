dfc171331c2fe7d4c6e9a16a74e4b6b10edced0e
ashishraj810 <ashishraj08105@gmail.com>
2025-06-13 23:52:21 +0530

    Add files via upload

---
+++ AnagramCheck.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class AnagramCheck {
+    public static boolean areAnagrams(String str1, String str2) {
+        // Remove spaces and convert to lowercase
+        str1 = str1.replaceAll("\\s", "").toLowerCase();
+        str2 = str2.replaceAll("\\s", "").toLowerCase();
+
+        // If lengths differ, they can't be anagrams
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        // Convert to character arrays and sort
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        // Compare sorted arrays
+        return Arrays.equals(arr1, arr2);
+    }
+
+    public static void main(String[] args) {
+        String s1 = "listen";
+        String s2 = "silent";
+
+        if (areAnagrams(s1, s2)) {
+            System.out.println(s1 + " and " + s2 + " are anagrams.");
+        } else {
+            System.out.println(s1 + " and " + s2 + " are not anagrams.");
+        }
+    }
+}
